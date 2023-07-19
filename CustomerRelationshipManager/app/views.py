bae181748f0fd4e6dadb51a6da53557d4312e3ab
debasish2110 <debasishdash98@gmail.com>
2023-07-20 03:04:45 +0530

    login logout functionality added

--- CustomerRelationshipManager/app/views.py
+++ CustomerRelationshipManager/app/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+###### FUNCTION BASED VIEWS ######
+
+def home(request):
+    # check login
+    if request.method == 'POST':
+        # fetching the username from post
+        # user_nameand password key is same as the name defined in the form input tag.
+        # home.html line no 15 and 18
+        username = request.POST['user_name']
+        password = request.POST['password']
+
+        #authenticating the user.
+        user = authenticate(request=request, username=username, password=password)
+
+        # need to define meesages in base.html
+        if user is not None:
+            # if user is authenticated then logging in the user
+            login(request=request, user=user)
+            messages.success(request=request, message="Log in successful")
+            # redirecting to the target view.
+            return redirect('app:Home')
+        else:
+            messages.MessageFailure(request=request, message="Log in Failed, Please try again.")
+            return redirect('app:Home')
+    else:
+        return render(request=request, template_name='home.html')
+
+# def login_user(request):
+#     pass
+
+def logout_user(request):
+    logout(request=request)
+    messages.success(request=request, message="Log out successful")
+    return redirect('app:Home')
+
