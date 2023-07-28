969ebc0413bc939235a998773867eac04df146ca
Debashish Dash <41960113+debasish2110@users.noreply.github.com>
2023-07-29 02:00:40 +0530

    Update

--- CustomerRelationshipManager/CustomerRelationshipManager/settings.py
+++ CustomerRelationshipManager/CustomerRelationshipManager/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'crm_db',
         'USER': 'root',
-        'PASSWORD': 'r00T#21!',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306'
     }
